682ac7397c84acefff1cb0976626b46fb690df6b
ilpo09 <ilja.pojanen@gmail.com>
2026-05-28 20:55:56 +0300

    Update RauhaTap offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,41 @@
-const CACHE_NAME = "apukortti-v1";
+const CACHE_NAME = "rauhatap-v2";
 
 const FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./service-worker.js",
+  "./rauhatap-circle-three-waves-small-cutout.svg"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
+  );
+});
